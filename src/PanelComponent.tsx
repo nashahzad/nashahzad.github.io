c170dc01fd30e43c12c43a027212842f931341dc
Nauman Shahzad <nashahzad@cs.stonybrook.edu>
2018-07-29 19:08:41 -0400

    Rectified DOM nesting of <p> and <ul>

--- src/PanelComponent.tsx
+++ src/PanelComponent.tsx
@@ -14,16 +14,14 @@ export default class PanelComponent extends React.Component<{projects: boolean},
                 <div className="card-body">
                     <h4 className="card-title">{panelContent.title}</h4>
                     <h6 className="card-subtitle mb-2 text-muted">{panelContent.startDate} - {panelContent.endDate}</h6>
-                    <p className="card-text">
-                        <ul>
+                    <ul className="card-text">
                         {panelContent.description.split('. ').map((sentence, i) =>{
                             if(sentence === ''){
                                 return '';
                             }
                             return <li key={"item_" + i}> {sentence} </li>
-                        })}
-                        </ul>
-                    </p>
+                        })}                        
+                    </ul>
                     {panelContent.href != null ? <a href={panelContent.href} className="card-link" target="_blank">{panelContent.hrefLabel}</a>: ''}
                 </div>
             </div>);
