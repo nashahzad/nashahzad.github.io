85b995810666dff3904487c7c903988f29f5dcfc
Nauman Shahzad <nauman.a.shahzad@gmail.com>
2023-08-22 16:32:36 -0400

    Updated experiences and descriptions

--- src/Constants.tsx
+++ src/Constants.tsx
@@ -31,22 +31,24 @@ export var experiences = new Array<Panel>();
 experiences.push({
     title: "Senior Software Engineer, Harver",
     startDate: "April 2019",
-    endDate: "Current",
-    description: "Python back end development in django and serverless. " +
-        "Created fully automated CI/CD pipelines for all of our microservices  with GoCD. " +
-        "Designed system integration testing flows to facilitate testing end to end User experience flows. " +
-        "Created technical specs for both internal and external users  with PlantUML and OpenAPI. " +
-        "Reduced incident resolution time by adding New Relic instrumentation and PagerDuty alerting. " +
-        "Architected a fan out message subscription service facilitating client’s ability to self subscribe to messages. " +
-        "Created an open source  CLI tool that utilizes Jira’s API for gathering various metrics on specified sprints.",
+    endDate: "July 2023",
+    description: "Lead team for architecting new event driven architecture for internal client subscriptions to system events to transition from legacy monolith design. Python, Celery, SQS, Lambda, Redis, and DynamoDB " +
+        "Tech lead for developing frameworks and APIs for scalable connectors with AWS Lambda and Serverless, working with business stakeholders, e.g.. Integration with Oracle and Workday " +
+        "Developed a third-party authorization system that enabled clients to manage, provision, and track access to their data. Python, Django, MySQL " +
+        "Created an open-source CLI tool for engineers that utilized Jira’s API and doubled our scrum teams’ ability to measure the efficiency of strategies from sprint to sprint. " +
+        "Developed end-to-end integration tests for critical path flows, reducing the need for manual testing. Python, Django, mySQL " +
+        "Created CI/CD pipelines with GoCD for all of our services, providing a way to support testing, track builds, and version control. " +
+        "Cut emergency incident resolution time in half by implementing New Relic and PagerDuty alerting. " +
+        "Worked with distributed teams; leading design discussions, code reviews, interviews, and mentoring of junior developers. ",
     href: null,
     hrefLabel: null
 });
 experiences.push({
     title: "Full Stack Web Developer, Eduware Inc.",
     startDate: "October 2017",
-    endDate: "May 2017, Part Time",
-    description: "ASP.NET Stack: C#, JQuery, LINQ, and Web Forms. Implement new features, bug fixes, and updates to legacy code",
+    endDate: "May 2017",
+    description: "Created and launched client-facing web app, using ASP.NET stack (C#, LINQ, Web Forms, AJAX) and Javascript/jQuery, enabling real-time test and assignment administration for teachers. " +
+        "Provided clean, concise, and scalable codebase for current and future developers by refactoring legacy back-end code in alignment with best practices for code design. ",
     href: null,
     hrefLabel: null
 });
@@ -54,8 +56,8 @@ experiences.push({
     title: "Software Engineer Intern, Bamtech LLC.",
     startDate: "January 2017",
     endDate: "August 2017",
-    description: "Back-end development with Go and Groovy. Created a REST service to interact with JFrog's Artifactory and Mission-Control APIs \
-    to automate artifactory creation, permissions set up, and push replication across every cluster. Utilized Docker to create containers for the REST service",
+    description: "Designed and implemented modularized REST API from scratch that supported various workflows and was easily extendable to accommodate all future use cases.  Go, Groovy " +
+        "Automated artifact creation, permissions setup, and push replication across clusters for software teams by creating REST service to interact with JFrog’s Artifactory and Mission-Control APIs. ",
     href: null,
     hrefLabel: null
 });
